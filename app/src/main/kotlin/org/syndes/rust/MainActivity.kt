57ae8c59432aaae9561b9e13fac5616089623e5b
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2026-01-16 13:13:47 +0700

    Update MainActivity.kt

--- app/src/main/kotlin/org/syndes/rust/MainActivity.kt
+++ app/src/main/kotlin/org/syndes/rust/MainActivity.kt
@@ -1,33 +1,13 @@
 package org.syndes.kotlincomponents
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.animation.AlphaAnimation
-import android.view.animation.Animation
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Устанавливаем layout с фоном и текстом
         setContentView(R.layout.activity_main)
-
-        val textRust = findViewById<TextView>(R.id.textRust)
-
-        // Лёгкая анимация (появление)
-        val fadeIn = AlphaAnimation(0f, 1f)
-        fadeIn.duration = 1500
-        fadeIn.repeatCount = Animation.INFINITE
-        fadeIn.repeatMode = Animation.REVERSE
-        textRust.startAnimation(fadeIn)
-
-        // Запуск перехода через 2 секунды
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, EditorActivity::class.java))
-            finish()
-        }, 2000)
     }
 }
