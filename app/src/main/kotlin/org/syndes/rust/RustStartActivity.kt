e277e3923c478a65d5045c016cde3906be07ae5a
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2026-01-16 12:54:26 +0700

    Create RustStartActivity.kt

---
+++ app/src/main/kotlin/org/syndes/rust/RustStartActivity.kt
@@ -0,0 +1,33 @@
+package org.syndes.kotlincomponents
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class RustStartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ruststart)
+
+        val textRust = findViewById<TextView>(R.id.textRust)
+
+        // Лёгкая анимация (появление)
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.duration = 1500
+        fadeIn.repeatCount = Animation.INFINITE
+        fadeIn.repeatMode = Animation.REVERSE
+        textRust.startAnimation(fadeIn)
+
+        // Запуск перехода через 2 секунды
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, EditorActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
