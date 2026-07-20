b392530444ec083c8011a8c2e3560685b4699ccb
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2026-07-21 00:18:39 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "0.18"
+        versionName = "1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
